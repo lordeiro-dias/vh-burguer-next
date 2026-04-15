9b33b89df5716b51b30a39e199185d9f829f81cd
lordeiro-dias <eduborgesbldias@gmail.com>
2026-04-15 11:01:23 -0300

    :tada: init: First Commit
    Início do Projeto do VH Burguer em NEXT, com TypeScript.

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <>
+        <p>AAAAAAAAAAAAAAAAAAAAAA</p>
+        </>
+    )
+}
+
+export default Home
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,34 @@
+import styles from './login.module.css';
+
+//ESTRUTURA PADRÃO
+const Login = () => {
+    return(
+        <>
+            <main id={styles.telaInteira}>
+                <img src="/imgs/hamburguer_login.png" alt="" />
+                <div id={styles.centralizarLogin}>
+
+                    <h1 className={styles.titulu}>Login</h1>
+
+                    <form action="" id={styles.divisaoFormulario}>
+                        <div className={styles.campo_form}>
+                            <label htmlFor="email">E-mail</label>
+                            <input className={styles.inputs} type="text" name="email" placeholder="email@exemplo.com" required/>
+                        </div>
+
+                        <div className={styles.campo_form}>
+                            <label htmlFor="senha">Senha</label>
+                            <input className={styles.inputs} type="password" name="senha" placeholder="********" required/>
+                        </div>
+                        
+                        <a id={styles.esqueceuSenha}href="">Esqueceu sua Senha?</a>
+                        <button className={styles.botaoEntrar}>Entrar</button>
+                    </form>
+
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Login;
